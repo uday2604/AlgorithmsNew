315ca892eb87ff4289446862d1094f69c401dce9
Uday Thota <udaydeep.thota@gmail.com>
2019-01-23 01:52:33 -0800

    add the code for 2 more meds

---
+++ LeetCode/src/main/java/med/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,56 @@
+package med;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by udaythota on 1/22/19.
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ * <p>
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * Example:
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * </p>
+ */
+public class LetterCombinationsPhoneNumber {
+
+    // NOTE: the key here is that, all the elements in the result set will have the length equal to the input string.
+    // so in the first iteration, add all the elements corresponding to the first digit, and in each of the next corresponding iterations, keep adding each character of the new digit array to each element of the previous result array
+    private static List<String> letterCombinations(String digits) {
+
+        List<String> resultList = new ArrayList<String>();
+        if (digits == null || digits.equals("") || digits.contains("1"))
+            return resultList;
+
+        String[] dataArray = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        for (int i = 0; i < digits.length(); i++) {
+            List<String> tempList = new ArrayList<String>();
+            char[] numCharArray = dataArray[digits.charAt(i) - '0'].toCharArray();
+
+            // so as the first iteration to make it succeed (when the result array is empty)
+            if (resultList.isEmpty()) {
+                resultList.add("");
+            }
+
+            // add all the new characters (from the current digit array) to all the elements of the current result array
+            for (String str : resultList) {
+                for (int j = 0; j < numCharArray.length; j++) {
+                    tempList.add(str + numCharArray[j]);
+                }
+            }
+            resultList = tempList;
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+        System.out.println(letterCombinations("456"));
+        System.out.println(letterCombinations("2"));
+        System.out.println(letterCombinations("231"));  // should return empty list as it contains '1'
+        System.out.println(letterCombinations(""));
+    }
+}
---
+++ LeetCode/src/main/java/med/LongestPalindromicSubString.java
@@ -0,0 +1,47 @@
+package med;
+
+/**
+ * Created by udaythota on 1/18/19.
+ */
+public class LongestPalindromicSubString {
+
+    public static String longestPalindrome(String testString) {
+
+        if (testString == null || testString.length() <= 1 || testString.equals(""))
+            return testString;
+
+        int startPointer = 0;
+        int endPointer = testString.length() - 1;
+        int resultStart = 0, resultEnd = 0;
+
+        while (startPointer <= endPointer) {
+            if (testString.charAt(startPointer) == testString.charAt(endPointer)) {
+                resultStart = Math.min(startPointer, resultStart);
+                resultEnd = Math.max(endPointer, resultEnd);
+            } else {
+                resultStart = resultEnd = 0;
+            }
+            startPointer++;
+            endPointer--;
+        }
+
+        String finalString = testString.substring(resultStart, resultEnd + 1);
+        if (finalString.charAt(0) != finalString.charAt(finalString.length() - 1)) {
+            return finalString.substring(1, finalString.length());
+        } else {
+            return finalString;
+        }
+
+        // if(testString.substring(resultStart, resultEnd).charAt(0)!=testString.substring(resultStart, resultEnd).charAt()
+        // return testString.substring(resultStart, resultEnd + 1);
+    }
+
+    public static void main(String[] args) {
+        // assertEquals(longestPalindrome("babad"), "bab");
+        //  assertEquals(longestPalindrome("abaaba"), "abaaba");
+        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("abaaba"));
+        System.out.println(longestPalindrome("madam"));
+        System.out.println(longestPalindrome("abb"));
+    }
+}
