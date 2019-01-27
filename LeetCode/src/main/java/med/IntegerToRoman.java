303f097eb0ed547f2efc1952e96098658761581d
Uday Thota <udaydeep.thota@gmail.com>
2019-01-27 02:50:23 -0800

    add 2 more meds

---
+++ LeetCode/src/main/java/med/IntegerToRoman.java
@@ -0,0 +1,69 @@
+package med;
+
+import java.util.*;
+
+/**
+ * Created by udaythota on 1/24/19.
+ */
+public class IntegerToRoman {
+
+    private static String intToRoman(int num) {
+
+        StringBuilder resultString = new StringBuilder();
+
+        HashMap<Integer, String> romanNumberMap = new HashMap<Integer, String>();
+        romanNumberMap.put(1, "I");
+        romanNumberMap.put(4, "IV");
+        romanNumberMap.put(5, "V");
+        romanNumberMap.put(9, "IX");
+        romanNumberMap.put(10, "X");
+        romanNumberMap.put(40, "XL");
+        romanNumberMap.put(50, "L");
+        romanNumberMap.put(90, "XC");
+        romanNumberMap.put(100, "C");
+        romanNumberMap.put(400, "CD");
+        romanNumberMap.put(500, "D");
+        romanNumberMap.put(900, "CM");
+        romanNumberMap.put(1000, "M");
+
+        if (num <= 0) {
+            return resultString.toString();
+        }
+
+        if (romanNumberMap.containsKey(num)) {
+            return romanNumberMap.get(num);
+        }
+
+        Set<Integer> keySetTemp = romanNumberMap.keySet();
+        Set<Integer> keySet = new TreeSet<Integer>(keySetTemp);
+
+        List<Integer> numberList = new ArrayList<Integer>(keySet);
+
+        int i = 0;
+        while (num != 0) {
+
+            while (num >= 1000) {
+                resultString.append("M");
+                num -= 1000;
+            }
+
+            if(num==0) {
+                return resultString.toString();
+            }
+
+            while (num >= numberList.get(i)) {
+                i++;
+            }
+
+
+            resultString.append(num == numberList.get(i) ? romanNumberMap.get(numberList.get(i)) : romanNumberMap.get(numberList.get(i - 1)));
+            num -= num == numberList.get(i) ? numberList.get(i) : numberList.get(i - 1);
+            i = 0;
+        }
+        return resultString.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(2000));
+    }
+}
---
+++ LeetCode/src/main/java/med/LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,63 @@
+package med;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by udaythota on 1/25/19.
+ * <p>
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Example 1:
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * </p>
+ */
+public class LongestSubstringWithoutRepeatingChars {
+
+    // NOTE: take care of the last case: if the loop exits without a repeating character, max length should be explicitly calculated
+    // Time Complexity: O(n^1), Space Complexity: O(1): as char array space is always a constant
+    private static int lengthOfLongestSubstring(String inputString) {
+
+        if (inputString == null || inputString.equals("") || inputString.length() < 1) {
+            return 0;
+        }
+
+        int maxLength = 1;
+        int startPointer = 0;
+        boolean[] charArray = new boolean[256];  // to take care of all the characters
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (charArray[inputString.charAt(i)]) {
+
+                char currentChar = inputString.charAt(i);
+                int currentLength = i - startPointer;
+
+                for (int j = startPointer; j < i; j++) {
+                    if (inputString.charAt(j) == currentChar) {
+                        startPointer = j + 1;
+                        break;
+                    }
+                }
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                }
+            } else {
+                charArray[inputString.charAt(i)] = true;
+            }
+        }
+
+        // NOTE: last case: when the loop exits, calculate the current length and reassign the max length if needed
+        maxLength = Math.max(maxLength, inputString.length() - startPointer);
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        assertEquals(lengthOfLongestSubstring("aabcadeaabf"), 5);
+        assertEquals(lengthOfLongestSubstring("bbbbb"), 1);
+        assertEquals(lengthOfLongestSubstring("abcdea"), 5);
+        assertEquals(lengthOfLongestSubstring("abcabcbb"), 3);
+        assertEquals(lengthOfLongestSubstring("aabdceb"), 5);
+        assertEquals(lengthOfLongestSubstring("au"), 2);
+        assertEquals(lengthOfLongestSubstring("aab"), 2);
+    }
+}
