772b66a06099b075f1f501dc2425d4ce34187328
Uday Thota <udaydeep.thota@gmail.com>
2019-06-01 23:19:42 -0700

    add the binary tree utils

---
+++ LeetCode/src/main/java/med/BinaryTreeUtils.java
@@ -0,0 +1,37 @@
+package med;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by udaythota on 6/1/19.
+ * Simple tree utility methods that can be reused
+ */
+public class BinaryTreeUtils {
+    // logic: simple BFS: get the queue size at each level, process and add them in to separate lists
+    protected static void printLevelOrder(TreeNode root) {
+        if (root == null) {
+            System.out.println("No elements in the tree.");
+        }
+        Queue<TreeNode> nodeStoreQueue = new LinkedList<>();
+        nodeStoreQueue.offer(root);
+
+        while (!nodeStoreQueue.isEmpty()) {
+            int queueSize = nodeStoreQueue.size();   // save the current queue size
+
+            for (int i = 0; i < queueSize; i++) {
+                TreeNode temp = nodeStoreQueue.poll();
+                System.out.print(temp.val + " ");
+
+                if (temp.left != null) {
+                    nodeStoreQueue.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    nodeStoreQueue.offer(temp.right);
+                }
+            }
+        }
+    }
+}
