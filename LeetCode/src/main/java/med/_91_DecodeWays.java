13376a8f340cce2b760e78f00c1af6916c1d4a34
Uday Thota <udaydeep.thota@gmail.com>
2019-06-06 01:43:19 -0700

    add the soln for decode ways

---
+++ LeetCode/src/main/java/med/_91_DecodeWays.java
@@ -0,0 +1,37 @@
+package med;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by udaythota on 6/5/19.
+ * <p>
+ * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+ * Given a non-empty string containing only digits, determine the total number of ways to decode it.
+ * </p>
+ */
+public class _91_DecodeWays {
+
+    // DP: Tabulation: Bottom Up approach
+    private static int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= s.length(); i++) {
+            int oneDigit = Integer.valueOf(s.substring(i - 1, i));
+            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
+            if (oneDigit >= 1) {   // all the mappings are valid for numbers>1
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {  // 2 digit combinations are valid only in this range
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        assertEquals(numDecodings("12"), 2);
+        assertEquals(numDecodings("226"), 3);
+        System.out.println(numDecodings("2264"));
+    }
+}
