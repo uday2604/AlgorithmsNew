e099489d26273aabf501eebfdc34493aa36a516a
Uday Thota <udaydeep.thota@gmail.com>
2019-06-15 20:08:51 -0700

    add the soln for course schedule

---
+++ LeetCode/src/main/java/med/_207_CourseSchedule.java
@@ -0,0 +1,67 @@
+package med;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by udaythota on 6/15/19.
+ * <p>
+ * There are a total of n courses you have to take, labeled from 0 to n-1.
+ * <p>
+ * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+ * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+ * </p>
+ */
+public class _207_CourseSchedule {
+    private static boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0 || prerequisites == null) {
+            return true;
+        }
+
+        List<List<Integer>> courses = new ArrayList<>();  // to represent a dependency graph
+        for (int i = 0; i < numCourses; i++) {
+            courses.add(new ArrayList<Integer>());
+        }
+
+        // create a dependency graph
+        for (int i = 0; i < prerequisites.length; i++) {
+            courses.get(prerequisites[i][1]).add(prerequisites[i][0]);   // prerequisites[i][j] -> represents i is dependent on j. so create a parent -> dependent child graph
+        }
+
+        int[] visited = new int[numCourses];  // to track the visited nodes. 0->not visited , 1-> being visited, 2-> visited
+
+        for (int i = 0; i < numCourses; i++) {  // iterate through all the courses and whenever there is a cycle in the dependent child's, return false
+            if (!dfs(i, courses, visited)) {
+                return false;
+            }
+        }
+        return true;   // means we successfully iterated through all the nodes and no cyclic loop is seen between the underlying courses, so course schedule is possible
+    }
+
+    // dfs helper: topological sort (which is equivalent to a pre order traversal)
+    private static boolean dfs(int course, List<List<Integer>> courses, int[] visited) {
+        visited[course] = 1;   // represents the node being visited
+        List<Integer> eligibleCourses = courses.get(course);   // get all the children for the current node
+
+        for (Integer eligibleCourse : eligibleCourses) {
+            if (visited[eligibleCourse] == 1) {  // has been re-visited while visiting its children - CYCLE !!!! (eg: 0->1 and 1->0)
+                return false;
+            }
+            if (visited[eligibleCourse] == 0) {  // node not visited: so preform dfs
+                if (!dfs(eligibleCourse, courses, visited)) {
+                    return false;
+                }
+            }
+        }
+        visited[course] = 2;  // mark the visited node
+        return true;
+    }
+
+    public static void main(String[] args) {
+        assertTrue(canFinish(2, new int[][]{{1, 0}}));
+        assertFalse(canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+    }
+}
