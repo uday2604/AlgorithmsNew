58dfbde19e57342a103c18f4cd972726359024a5
Uday Thota <udaydeep.thota@gmail.com>
2019-06-23 16:33:20 -0700

    add linked list utils to easy package

---
+++ LeetCode/src/main/java/easy/LinkedListUtils.java
@@ -0,0 +1,39 @@
+package easy;
+
+/**
+ * Created by udaythota on 6/23/19.
+ * Commonly used linked list utility methods so as to reuse
+ */
+public class LinkedListUtils {
+
+    private ListNode head;
+
+    protected static class ListNode {
+        protected int val;
+        protected ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    protected void addToTheLast(ListNode listNode) {
+        if (head == null) {
+            head = listNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = listNode;
+        }
+    }
+
+    protected void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
