98ce96a8d02e14274853c7819cf9046a2290f34a
Uday Thota <udaydeep.thota@gmail.com>
2019-07-03 23:06:52 -0700

    add the tree node structure in an other package

---
+++ LeetCode/src/main/java/easy/TreeNode.java
@@ -0,0 +1,17 @@
+package easy;
+
+/**
+ * Created by udaythota on 7/3/19.
+ * Basic Tree Node structure
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
