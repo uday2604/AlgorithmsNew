bb7cbab11776ec61bce6cb9dbc43e930d59ff0ac
Uday Thota <udaydeep.thota@gmail.com>
2019-07-06 18:26:02 -0700

    add the soln for excel sheet colm number

---
+++ LeetCode/src/main/java/easy/_171_Excel_Sheet_Column_Number.java
@@ -0,0 +1,47 @@
+package easy;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by udaythota on 7/6/19.
+ * <p>
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * </p>
+ */
+public class _171_Excel_Sheet_Column_Number {
+
+    // core logic: this is similar to binary to decimal conversion
+    // example 1: CDA -> 26^2 * 3 + 26^1 * 4 + 26^0 * 1
+    // example 2: ZY -> 26^1 * 26 + 26^0 * 25
+    private static int titleToNumber(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int charDigit = s.charAt(s.length() - 1 - i) - 'A' + 1;
+            result += Math.pow(26, i) * charDigit;
+        }
+        return result;
+    }
+
+    // similar to above approach, just a different style of writing (no need of math.pow)
+    private static int titleToNumber2(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // test case: 1
+        assertEquals(titleToNumber("A"), 1);
+        assertEquals(titleToNumber("AB"), 28);
+        assertEquals(titleToNumber("ZY"), 701);
+        assertEquals(titleToNumber("CDA"), 2133);
+
+        // test case: 2
+        assertEquals(titleToNumber2("A"), 1);
+        assertEquals(titleToNumber2("AB"), 28);
+        assertEquals(titleToNumber2("ZY"), 701);
+        assertEquals(titleToNumber2("CDA"), 2133);
+    }
+}
